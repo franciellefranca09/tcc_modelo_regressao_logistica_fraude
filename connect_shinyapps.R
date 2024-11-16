1bd049f44d1f9c61bface154b64505e637ad5b38
Fran França <92217790+franciellefranca09@users.noreply.github.com>
2024-11-15 22:51:12 -0300

    Update connect_shinyapps.R

--- connect_shinyapps.R
+++ connect_shinyapps.R
@@ -1,8 +1,11 @@
-install.packages('rsconnect')
+# install.packages('rsconnect') (Instalar caso ainda não tenha o pacote no RStudio).
+# Fazer o cadastro de usuário no Shinyapps.io, ao final o serviço irá prover as informações abaixo.
 
 rsconnect::setAccountInfo(name='****',
                           token='***',
                           secret='**')
+
+# Copiar o caminho do Diretório onde está o Shiny app, assim como o seu nome.
 
 library(rsconnect)
 
